a6a5edf91e05726aef746d798c65987d84b7a7e3
Xejb <vladiq-artamonenko@rambler.ru>
2022-11-02 16:06:49 +0300

    name

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ bot = telebot.TeleBot('5457216018:AAHtHe5l0jY78oPK8sVGVb7FDOt6JJ9kgzY')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '<b>hi dude</b>', parse_mode='html')
+    mess = f'Hi, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>'
+    bot.send_message(message.chat.id, mess, parse_mode='html')
 
 
 bot.polling(none_stop=True)
