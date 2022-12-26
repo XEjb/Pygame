3543874f866887c0ac0f02b440b8658ec52f998c
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-26 18:53:04 +0300

    refactoring

--- app/main.py
+++ app/main.py
@@ -3,25 +3,14 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 300))
 pygame.display.set_caption('Nauty Game')
-icon = pygame.image.load('images/icon.png')
-pygame.display.set_icon(icon)
-
-
-square = pygame.Surface((50, 170))
-square.fill('Blue')
-
-myfont = pygame.font.Font('fonts/Sevillana-Regular.ttf', 40)
-text_surface = myfont.render('Nauty', True, 'Red')
-
+# icon = pygame.image.load('images/icon.png')
+# pygame.display.set_icon(icon)
 
 player = pygame.image.load('images/icon.png')
 
 run = True
 while run:
 
-    pygame.draw.circle(screen, 'Red', (10, 7), 5)
-    screen.blit(square, (10, 0))
-    screen.blit(text_surface, (300, 100))
     screen.blit(player, (100, 50))
 
     pygame.display.update()
