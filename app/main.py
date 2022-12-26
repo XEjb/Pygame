f10e4094ee9f5b1ce576945ac50284e3aae06870
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-26 18:46:07 +0300

    Adding and locating objects

--- app/main.py
+++ app/main.py
@@ -3,13 +3,26 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 300))
 pygame.display.set_caption('Nauty Game')
-# icon = pygame.image.load('images/icon.png')
-# pygame.display.set_icon(icon)
+icon = pygame.image.load('images/icon.png')
+pygame.display.set_icon(icon)
+
+
+square = pygame.Surface((50, 170))
+square.fill('Blue')
+
+myfont = pygame.font.Font('fonts/Sevillana-Regular.ttf', 40)
+text_surface = myfont.render('Nauty', True, 'Red')
+
+
+player = pygame.image.load('images/icon.png')
 
 run = True
 while run:
 
-    # screen.fill((252, 152, 3))
+    pygame.draw.circle(screen, 'Red', (10, 7), 5)
+    screen.blit(square, (10, 0))
+    screen.blit(text_surface, (300, 100))
+    screen.blit(player, (100, 50))
 
     pygame.display.update()
 
@@ -17,6 +30,3 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                screen.fill((140, 3, 252))
