f3c8a39861b5dd8b5da7dd7d1406a68a0585d844
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-27 15:09:34 +0300

    timer, displaying enemies on the screen

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ clock = pygame.time.Clock()
 pygame.init()
 screen = pygame.display.set_mode((618, 359))
 pygame.display.set_caption('Nauty Game')
-icon = pygame.image.load('images/icon.png')
+icon = pygame.image.load('images/icon.png').convert_alpha()
 pygame.display.set_icon(icon)
 
-bg = pygame.image.load('images/fon.jpg')
+# Player
+bg = pygame.image.load('images/fon.jpg').convert_alpha()
 walk_left = [
-    pygame.image.load('images/left.png'),
-    pygame.image.load('images/runleft.png'),
+    pygame.image.load('images/left.png').convert_alpha(),
+    pygame.image.load('images/runleft.png').convert_alpha(),
 ]
 walk_right = [
-    pygame.image.load('images/right.png'),
-    pygame.image.load('images/runright.png'),
+    pygame.image.load('images/right.png').convert_alpha(),
+    pygame.image.load('images/runright.png').convert_alpha(),
 ]
+
+ghost = pygame.image.load('images/enemy.png').convert_alpha()
+ghost_list_in_game = []
 
 player_anim_count = 0
 bg_x = 0
@@ -26,7 +30,10 @@ player_x = 150
 player_y = 250
 
 is_jump = False
-jump_count = 7
+jump_count = 8
+
+ghost_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(ghost_timer, 2500)
 
 run = True
 while run:
@@ -34,12 +41,22 @@ while run:
     screen.blit(bg, (bg_x, 0))
     screen.blit(bg, (bg_x + 618, 0))
 
-    keys = pygame.key.get_pressed()()
+    player_rect = walk_left[0].get_rect(topleft=(player_x, player_y))
+
+    if ghost_list_in_game:
+        for el in ghost_list_in_game:
+            screen.blit(ghost, el)
+            el.x -= 10
+
+            if player_rect.colliderect(el):
+                print('You lose')
+
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         screen.blit(walk_left[player_anim_count], (player_x, player_y))
     else:
         screen.blit(walk_right[player_anim_count], (player_x, player_y))
-    keys = pygame.key.get_pressed()
+
     if keys[pygame.K_LEFT] and player_x > 50:
         player_x -= player_speed
     elif keys[pygame.K_RIGHT] and player_x < 200:
@@ -49,7 +66,7 @@ while run:
         if keys[pygame.K_SPACE]:
             is_jump = True
     else:
-        if jump_count >= -7:
+        if jump_count >= -8:
             if jump_count > 0:
                 player_y -= (jump_count ** 2) / 2
             else:
@@ -57,7 +74,7 @@ while run:
             jump_count -= 1
         else:
             is_jump = False
-            jump_count = 7
+            jump_count = 8
 
     if player_anim_count == 3:
         player_anim_count = 0
@@ -74,5 +91,7 @@ while run:
         if event.type == pygame.QUIT:
             run = False
             pygame.quit()
+        if event.type == ghost_timer:
+            ghost_list_in_game.append(ghost.get_rect(topleft=(620, 250)))
 
     clock.tick(20)
