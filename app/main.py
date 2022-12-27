86be6a8e08529df2ed3f9a4c71419eb733942365
Xejb <vladiq-artamonenko@rambler.ru>
2022-12-27 14:19:30 +0300

    jump

--- app/main.py
+++ app/main.py
@@ -23,6 +23,10 @@ bg_x = 0
 
 player_speed = 5
 player_x = 150
+player_y = 250
+
+is_jump = False
+jump_count = 7
 
 run = True
 while run:
@@ -32,14 +36,28 @@ while run:
 
     keys = pygame.key.get_pressed()()
     if keys[pygame.K_LEFT]:
-        screen.blit(walk_left[player_anim_count], (player_x, 250))
+        screen.blit(walk_left[player_anim_count], (player_x, player_y))
     else:
-        screen.blit(walk_right[player_anim_count], (player_x, 250))
+        screen.blit(walk_right[player_anim_count], (player_x, player_y))
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and player_x > 50:
         player_x -= player_speed
     elif keys[pygame.K_RIGHT] and player_x < 200:
         player_x += player_speed
+
+    if not is_jump:
+        if keys[pygame.K_SPACE]:
+            is_jump = True
+    else:
+        if jump_count >= -7:
+            if jump_count > 0:
+                player_y -= (jump_count ** 2) / 2
+            else:
+                player_y += (jump_count ** 2) / 2
+            jump_count -= 1
+        else:
+            is_jump = False
+            jump_count = 7
 
     if player_anim_count == 3:
         player_anim_count = 0
